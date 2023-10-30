59a597df2f2edf4897555ec91367215128ee2dee
Saleihin Khan <saleihinkhan@gmail.com>
2023-10-30 01:42:38 -0700

    both sender and reciever conversation design Done in css file

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Message = () => {
     return (
-        <div className='message'>
+        <div className='message owner'>
             <div className="messageInfo">
                 <img src="https://scontent.fdac37-1.fna.fbcdn.net/v/t39.30808-6/396553076_1529197534565566_6679841638574927980_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-m9DwrxHejgAX-0K4d5&_nc_ht=scontent.fdac37-1.fna&oh=00_AfCf0MohlebbdYc58m_hY17GCUBqKgSXX-_-bA-mmWqe0A&oe=6543C912" alt="" />
                 <span>Just Now</span>
             </div>
             <div className="messageContent">
                 <p>Hello There! </p>
-                {/* <img src="https://scontent.fdac37-1.fna.fbcdn.net/v/t39.30808-6/396553076_1529197534565566_6679841638574927980_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-m9DwrxHejgAX-0K4d5&_nc_ht=scontent.fdac37-1.fna&oh=00_AfCf0MohlebbdYc58m_hY17GCUBqKgSXX-_-bA-mmWqe0A&oe=6543C912" alt="" /> */}
+                <img src="https://scontent.fdac37-1.fna.fbcdn.net/v/t39.30808-6/396553076_1529197534565566_6679841638574927980_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-m9DwrxHejgAX-0K4d5&_nc_ht=scontent.fdac37-1.fna&oh=00_AfCf0MohlebbdYc58m_hY17GCUBqKgSXX-_-bA-mmWqe0A&oe=6543C912" alt="" />
             </div>
         </div>
     );
