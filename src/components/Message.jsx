1dacf1b80afe45ef31cc11acd8fb8d52de902a56
Saleihin Khan <saleihinkhan@gmail.com>
2023-10-30 01:10:40 -0700

    Scrolling Messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Message = () => {
     return (
-        <div>
-            This is a Message
+        <div className='message'>
+            <div className="messageInfo">
+                <img src="https://scontent.fdac37-1.fna.fbcdn.net/v/t39.30808-6/396553076_1529197534565566_6679841638574927980_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-m9DwrxHejgAX-0K4d5&_nc_ht=scontent.fdac37-1.fna&oh=00_AfCf0MohlebbdYc58m_hY17GCUBqKgSXX-_-bA-mmWqe0A&oe=6543C912" alt="" />
+                <span>Just Now</span>
+            </div>
+            <div className="messageContent">
+                <p>Hello There! </p>
+                {/* <img src="https://scontent.fdac37-1.fna.fbcdn.net/v/t39.30808-6/396553076_1529197534565566_6679841638574927980_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-m9DwrxHejgAX-0K4d5&_nc_ht=scontent.fdac37-1.fna&oh=00_AfCf0MohlebbdYc58m_hY17GCUBqKgSXX-_-bA-mmWqe0A&oe=6543C912" alt="" /> */}
+            </div>
         </div>
     );
 };
