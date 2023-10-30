956d6d46666b8b290d10327e2e5b055e1a9a885a
Saleihin Khan <saleihinkhan@gmail.com>
2023-10-29 19:07:49 -0700

    login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Add from '../img/addAvatar.png';
+
+const Login = () => {
+    return (
+        <div className="formContainer">
+
+            <div className="formWrapper">
+
+                <span className="logo">TextAholic</span>
+                <span className="title">Register</span>
+                <form action="">
+                    <input type="text" placeholder='Display Name' />
+                    <input type="email" placeholder='Email Address' name="" id="" />
+                    <input type="password"  placeholder='Password' name="" id="" />
+                    <input style={{display:"none"}} type="file" name="" id="file" />
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an Avatar</span>
+                    </label>
+                   <button>Sign Up</button>
+
+                </form>
+
+                <p>Already Have an Account? Login </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Login;
